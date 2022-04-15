5c747c0b1e164e71d0b83c045c4e3364631f5752
korchinmv <korchinmv@yandex.ru>
2022-04-15 11:59:30 +0300

    added contacts page

--- app/js/main.js
+++ app/js/main.js
@@ -127,4 +127,15 @@ $(function () {
 
 	const deadline = $('.promo-clock').attr('data-time');
 	initializeClock('promo-clock', deadline);
+
+	let map;
+
+	function initMap() {
+		map = new google.maps.Map(document.getElementById("map"), {
+			center: { lat: -34.397, lng: 150.644 },
+			zoom: 8,
+		});
+	}
+
+	window.initMap = initMap;
 });
